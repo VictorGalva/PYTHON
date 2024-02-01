62b9ae2f80651475eabf79c10170aa4a1dcf14c0
Victor Galvão <farias.victor2004@gmail.com>
2024-02-01 16:33:02 -0300

    Create sequencia_de_fibonacci.py

---
+++ exercicios/sequencia_de_fibonacci.py
@@ -0,0 +1,16 @@
+print('-=-'*10)
+print('{: ^30}'.format('Sequencia de Fibonacci'))
+print('-=-'*10,'\n')
+qtde = int(input('Quantos números da sequência você deseja ver? '))
+termo1 = 0
+termo2 = 1
+print('{}→'.format(termo1), end = ' ')
+print('{}→'.format(termo2), end = ' ')
+i = 0
+while i < (qtde - 2):
+	prox = termo1 + termo2
+	print('{} →'.format(prox), end = ' ')
+	termo1 = termo2 
+	termo2 = prox
+	i += 1
+print('FIM')
